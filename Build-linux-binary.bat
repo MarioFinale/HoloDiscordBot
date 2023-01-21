c2fef92e66b5a1e6c809936830da20e2ab393dd8
MarioFinale <mariofinale@hotmail.com>
2023-01-20 21:53:38 -0300

    Fix build batch.

--- Build-linux-binary.bat
+++ Build-linux-binary.bat
@@ -1,3 +1,3 @@
-dotnet publish -c Release -r linux-x64 --self-contained /p:PublishSingleFile=true /p:PublishSelfContained=true -o ../result/PeriodiBOT/unix/
+dotnet publish -c Release -r linux-x64 --self-contained /p:PublishSingleFile=true /p:PublishSelfContained=true -o ./bin/unix/
 cd ..
 pause
